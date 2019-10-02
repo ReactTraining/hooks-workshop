92aa24ccfc7bf384202cf1453587cf358deeadd3
David Khourshid <davidkpiano@gmail.com>
2019-10-02 06:15:51 -0400

    chore: add About test

---
+++ modules/app/About.test.final.js
@@ -0,0 +1,43 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import About from "./About"
+
+describe("About", () => {
+  it("should render", () => {
+    const { getByText } = render(<About />)
+
+    const supportMessage = getByText(/you can do it/i)
+  })
+
+  it("should match inline snapshot", () => {
+    const { container } = render(<About />)
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="about"
+        >
+          <h1
+            style="text-align: center;"
+          >
+            8k
+          </h1>
+          <p>
+            It's simple. Exercise for 8,000 minutes this year.
+          </p>
+          <p>
+            That's 30 minutes a day, 5 days a week.
+          </p>
+          <p>
+            Track your progress here, and share it with others.
+          </p>
+          <p>
+            You can do it!
+          </p>
+        </div>
+      </div>
+    `)
+  })
+})
---
+++ modules/app/About.test.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import About from "./About"
+
+describe("About", () => {
+  it("should render", () => {
+    // assertion goes here
+  })
+
+  it("should match snapshot", () => {
+    // assertion goes here
+  })
+})
