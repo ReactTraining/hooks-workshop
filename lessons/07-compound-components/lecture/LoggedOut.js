5e635e56bc44f31bd9df0428414e361420e092e6
Cassidy Williams <cassidoo@gmail.com>
2019-11-19 10:34:14 -0600

    Add compount components lecture

--- lessons/07-compound-components/lecture/LoggedOut.js
+++ lessons/07-compound-components/lecture/LoggedOut.js
@@ -1,35 +1,168 @@
-import React, { useState, useContext, createContext, Children } from "react"
+import React, {
+  useState,
+  useContext,
+  createContext,
+  Children
+} from "react"
 
 import LoginForm from "app/LoginForm"
 import SignupForm from "app/SignupForm"
 import About from "app/About"
 
-function Tabs({ data }) {
+// npm start lecture
+// 7
+
+// All the stuff from yesterday is
+// in the `fall-tour-chicago` branch
+// maybe pull that later and not now heh
+
+const TabsContext = createContext()
+
+function Tabs({ children }) {
   const [activeIndex, setActiveIndex] = useState(0)
+  const [switched, setSwitched] = useState(false)
+
+  let arr = children.slice()
 
   return (
-    <div data-reach-tabs>
-      <div data-reach-tab-list>
-        {data.map((tab, index) => {
-          const isActive = index === activeIndex
-          return (
-            <div
-              data-reach-tab
-              key={index}
-              className={isActive ? "active" : ""}
-              onClick={() => setActiveIndex(index)}
-            >
-              {tab.label}
-            </div>
-          )
-        })}
+    <TabsContext.Provider
+      value={{
+        activeIndex,
+        setActiveIndex,
+        switched,
+        setSwitched
+      }}
+    >
+      <div data-reach-tabs>
+        {switched ? arr.reverse() : children}
       </div>
-      <div data-reach-tab-panels>{data[activeIndex].content}</div>
+    </TabsContext.Provider>
+  )
+}
+
+const TabListContext = createContext()
+
+function TabList({ children }) {
+  return (
+    <div data-reach-tab-list>
+      {Children.map(children, (child, index) => (
+        <TabListContext.Provider
+          key={index}
+          value={{ index }}
+        >
+          {child}
+        </TabListContext.Provider>
+      ))}
     </div>
   )
 }
 
+function Tab({ children, disabled }) {
+  const { index } = useContext(TabListContext)
+  const { activeIndex, setActiveIndex } = useContext(
+    TabsContext
+  )
+  const isActive = index === activeIndex
+
+  return (
+    <div
+      data-reach-tab
+      className={
+        disabled ? "disabled" : isActive ? "active" : ""
+      }
+      onClick={() => {
+        if (!disabled) setActiveIndex(index)
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+function TabPanels({ children }) {
+  const { activeIndex } = useContext(TabsContext)
+
+  return (
+    <div data-reach-tab-panels>{children[activeIndex]}</div>
+  )
+}
+
+function SwitchButton() {
+  const { switched, setSwitched } = useContext(TabsContext)
+  return (
+    <button
+      className="button"
+      onClick={() => {
+        setSwitched(!switched)
+      }}
+    >
+      SWITCH THE TABS
+    </button>
+  )
+}
+
+function TabPanel({ children }) {
+  return children
+}
+
+function DataTabs({
+  data,
+  tabsPosition = "top",
+  disabled = []
+}) {
+  const tabs = (
+    <TabList>
+      {data.map((item, index) => (
+        <Tab
+          key={index}
+          disabled={disabled.includes(index)}
+        >
+          {item.label}
+        </Tab>
+      ))}
+    </TabList>
+  )
+
+  const panels = (
+    <TabPanels>
+      {data.map((item, index) => (
+        <TabPanel key={index}>{item.content}</TabPanel>
+      ))}
+    </TabPanels>
+  )
+
+  return (
+    <Tabs>
+      {tabsPosition === "bottom"
+        ? [panels, tabs]
+        : [tabs, panels]}
+    </Tabs>
+  )
+}
+
 export default function LoggedOut() {
+  return (
+    <div className="LoggedOut">
+      <About />
+      <Tabs>
+        <TabList>
+          <Tab>Login</Tab>
+          <Tab>Signup</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SwitchButton />
+            <LoginForm />
+          </TabPanel>
+          <TabPanel>
+            <SwitchButton />
+            <SignupForm />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  )
+
   const tabData = [
     {
       label: "Login",
@@ -44,7 +177,11 @@ export default function LoggedOut() {
   return (
     <div className="LoggedOut">
       <About />
-      <Tabs data={tabData} />
+      <DataTabs
+        data={tabData}
+        tabsPosition="bottom"
+        disabled={[1]}
+      />
     </div>
   )
 }
