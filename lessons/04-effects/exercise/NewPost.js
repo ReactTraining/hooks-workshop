b123e403b635c60f1e27561bcefdd137ede4f94a
Cassidy Williams <cassidoo@gmail.com>
2019-12-11 13:54:14 -0800

    Add effects lecture

--- lessons/04-effects/exercise/NewPost.js
+++ lessons/04-effects/exercise/NewPost.js
@@ -1,28 +1,59 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { FaDumbbell } from 'react-icons/fa'
+import React, { useState, useEffect, useRef } from "react"
+import { FaDumbbell } from "react-icons/fa"
 
-import { useAppState } from 'app/app-state'
-import { formatDate, DATE_FORMAT } from 'app/utils'
-import Avatar from 'app/Avatar'
-import Minutes from 'app/Minutes'
-import RecentPostsDropdown from 'app/RecentPostsDropdown'
+import { useAppState } from "app/app-state"
+import { formatDate, DATE_FORMAT } from "app/utils"
+import Avatar from "app/Avatar"
+import Minutes from "app/Minutes"
+import RecentPostsDropdown from "app/RecentPostsDropdown"
 
 const MAX_MESSAGE_LENGTH = 200
 
-export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
+function useLocalStorage(key, message) {
+  useEffect(() => {
+    setLocalStorage(key, message)
+  }, [message])
+}
+
+// Empty array = on mount
+// Populated array = on mount + whenever those vars change
+// No array = on mount + whenever anything changes
+
+export default function NewPost({
+  takeFocus,
+  date,
+  onSuccess,
+  showAvatar
+}) {
   const [{ auth }] = useAppState()
-  const [message, setMessage] = useState('Ran around the lake.')
+  const key = makeNewPostKey(date)
+  const [message, setMessage] = useState(
+    getLocalStorageValue(key) || ""
+  )
   const messageTooLong = message.length > MAX_MESSAGE_LENGTH
+
+  const messageRef = useRef()
+
+  useEffect(() => {
+    if (takeFocus) messageRef.current.focus()
+  }, [takeFocus, message])
+
+  useLocalStorage(key, message)
 
   function handleMessageChange(event) {
     setMessage(event.target.value)
   }
 
   return (
-    <div className={'NewPost' + (messageTooLong ? ' NewPost_error' : '')}>
+    <div
+      className={
+        "NewPost" + (messageTooLong ? " NewPost_error" : "")
+      }
+    >
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
       <form className="NewPost_form">
         <textarea
+          ref={messageRef}
           className="NewPost_input"
           placeholder="Tell us about your workout!"
           value={message}
@@ -31,11 +62,17 @@ export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
         <div className="NewPost_char_count">
           {message.length}/{MAX_MESSAGE_LENGTH}
         </div>
-        <RecentPostsDropdown uid={auth.uid} onSelect={setMessage} />
+        <RecentPostsDropdown
+          uid={auth.uid}
+          onSelect={setMessage}
+        />
         <div className="NewPost_buttons">
           <Minutes date={date} />
           <div>
-            <button type="submit" className="icon_button cta">
+            <button
+              type="submit"
+              className="icon_button cta"
+            >
               <FaDumbbell /> <span>Post</span>
             </button>
           </div>
