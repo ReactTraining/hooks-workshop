b04db48600265d8574d32fac9732c04870cb24f3
Cassidy Williams <cassidoo@gmail.com>
2019-12-12 10:35:28 -0800

    Add compound components

--- lessons/07-compound-components/lecture/LoggedOut.js
+++ lessons/07-compound-components/lecture/LoggedOut.js
@@ -1,30 +1,65 @@
-import React, { useState, useContext, createContext, Children } from "react"
+import React, {
+  useState,
+  useContext,
+  createContext,
+  Children
+} from "react"
 
 import LoginForm from "app/LoginForm"
 import SignupForm from "app/SignupForm"
 import About from "app/About"
 
-function Tabs({ data }) {
+// We're going to be doing lecture 7 next!
+// npm start lecture
+// 7
+// LoggedOut.js in 07-compound-components
+
+function Tabs({
+  data,
+  tabPosition = "top",
+  disabled = []
+}) {
   const [activeIndex, setActiveIndex] = useState(0)
+
+  const tabs = (
+    <div data-reach-tab-list>
+      {data.map((tab, index) => {
+        const isActive = index === activeIndex
+        const isDisabled = disabled.includes(index)
+
+        return (
+          <div
+            data-reach-tab
+            key={index}
+            className={
+              isDisabled
+                ? "disabled"
+                : isActive
+                ? "active"
+                : ""
+            }
+            onClick={() => {
+              if (!isDisabled) setActiveIndex(index)
+            }}
+          >
+            {tab.label}
+          </div>
+        )
+      })}
+    </div>
+  )
+
+  const panels = (
+    <div data-reach-tab-panels>
+      {data[activeIndex].content}
+    </div>
+  )
 
   return (
     <div data-reach-tabs>
-      <div data-reach-tab-list>
-        {data.map((tab, index) => {
-          const isActive = index === activeIndex
-          return (
-            <div
-              data-reach-tab
-              key={index}
-              className={isActive ? "active" : ""}
-              onClick={() => setActiveIndex(index)}
-            >
-              {tab.label}
-            </div>
-          )
-        })}
-      </div>
-      <div data-reach-tab-panels>{data[activeIndex].content}</div>
+      {tabPosition === "bottom"
+        ? [panels, tabs]
+        : [tabs, panels]}
     </div>
   )
 }
@@ -44,7 +79,11 @@ export default function LoggedOut() {
   return (
     <div className="LoggedOut">
       <About />
-      <Tabs data={tabData} />
+      <Tabs
+        data={tabData}
+        tabPosition="bottom"
+        disabled={[1]}
+      />
     </div>
   )
 }
